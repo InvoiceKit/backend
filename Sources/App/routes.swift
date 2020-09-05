f243bb01fc7566767e24b8ac10796b2a6765cfe2
Victor Lourme <victor.lourme@icloud.com>
2020-09-05 10:46:27 +0200

    backend(routes): added CORS

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,36 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    // CORS
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [
+            .GET,
+            .POST,
+            .PUT,
+            .OPTIONS,
+            .DELETE,
+            .PATCH
+        ],
+        allowedHeaders: [
+            .accept,
+            .authorization,
+            .contentType,
+            .origin,
+            .xRequestedWith,
+            .userAgent,
+            .accessControlAllowOrigin
+        ]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    let error = ErrorMiddleware.default(environment: app.environment)
+    
+    // Clear any existing middleware.
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(error)
+    
+    // Routes
     try app.register(collection: TeamController())
     try app.register(collection: CustomerController())
     try app.register(collection: InvoiceController())
